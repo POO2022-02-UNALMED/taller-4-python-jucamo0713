c5b22bc291353af4bbde206dac5c0bd55e1fee81
jucamo0713 <jucamo.0713@gmail.com>
2022-09-05 06:04:50 -0500

    finish

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,37 +1,29 @@
-
-
 from classroom.asignatura import Asignatura
 from classroom.asignatura import Asignatura
 
 
 class Grupo:
 
-    grado = None
-
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+        self.grado = "Grado 12";
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
+        if (len(lista) > 0):
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
             self.listadoAlumnos = [alumno]
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    def __str__(self):
+        return "Grupo de estudiantes: grupo predeterminado"
